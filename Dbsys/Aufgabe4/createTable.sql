dbf0696afe282aebf7bf7a0df2f4da2a258b8635
Janis1310 <janis1310@gmail.com>
2024-11-18 10:35:28 +0100

    Fertige Lösung

--- Dbsys/Aufgabe4/createTable.sql
+++ Dbsys/Aufgabe4/createTable.sql
@@ -1,3 +1,50 @@
+CREATE TABLE Ausstattung
+(
+    bezeichnung VARCHAR2(255) PRIMARY KEY
+);
+
+CREATE TABLE Touristenattraktion
+(
+    bezeichnung VARCHAR2(255) PRIMARY KEY,
+    beschreibung VARCHAR2(255) --Kann attribut
+);
+
+CREATE TABLE Land 
+(
+    bezeichnung VARCHAR2(255) PRIMARY KEY
+);
+CREATE TABLE Adresse
+(
+    adressnr INT PRIMARY KEY,
+    strasse VARCHAR2(255) NOT NULL,
+    hausnummer INT NOT NULL,
+        CONSTRAINT hausnummer_format CHECK (REGEXP_LIKE(hausnummer, '^[0-9]{1,5}$')),
+    plz INT NOT NULL CHECK (plz > 0),
+    ort VARCHAR2(255) NOT NULL CHECK (LENGTH(ort) > 2),
+    landbezeichnung VARCHAR2(255) NOT NULL,
+    CONSTRAINT adresse_fk FOREIGN KEY (landbezeichnung) REFERENCES Land(bezeichnung)
+);
+
+
+
+CREATE TABLE Kunde
+(
+    email VARCHAR2(255) PRIMARY KEY, 
+        CONSTRAINT email_format CHECK (REGEXP_LIKE(email, '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')),
+    vorname VARCHAR2(255) NOT NULL,
+    nachname VARCHAR2(255) NOT NULL,
+    IBAN VARCHAR2(255) NOT NULL,
+        constraint IBAN_foramat CHECK (REGEXP_LIKE(IBAN, '^[A-Z]{2}[0-9]{2}[A-Z0-9]{4}[0-9]{7}([A-Z0-9]?){0,16}$')),
+    passwort VARCHAR2(255) NOT NULL,
+    adressnr INT NOT NULL,
+    CONSTRAINT kunde_fk FOREIGN KEY (adressnr) REFERENCES Adresse(adressnr)
+);
+
+
+
+
+
+
 CREATE TABLE Ferienwohnung
 (
     ferienwohnungsName VARCHAR2(255) PRIMARY KEY CHECK (LENGTH(ferienwohnungsName) > 3),
@@ -7,31 +54,11 @@ CREATE TABLE Ferienwohnung
     adressnr INT NOT NULL,
     CONSTRAINT ferienwohnung_fk FOREIGN KEY (adressnr) REFERENCES Adresse(adressnr) -- Standartmaessig ON DELETE RESTRICT
 );
-
 CREATE TABLE Bilder
 (
     url VARCHAR2(255) PRIMARY KEY,
     ferienwohnungsname VARCHAR2(255) NOT NULL,
     CONSTRAINT bilder_fk FOREIGN KEY (ferienwohnungsname) REFERENCES Ferienwohnung(ferienwohnungsname)
-);
-CREATE TABLE Ausstattung
-(
-    bezeichnung VARCHAR2(255) PRIMARY KEY
-);
-
-CREATE TABLE Besitzt
-(
-    ausstatungsbezeichnung VARCHAR2(255) NOT NULL,
-    ferienwohnungsname VARCHAR2(255) NOT NULL CHECK (LENGTH(ferienwohnungsname) > 3),
-    CONSTRAINT besitzt_pk PRIMARY KEY (ausstatungsbezeichnung, ferienwohnungsname),
-    CONSTRAINT besitzt_fk1 FOREIGN KEY (ausstatungsbezeichnung) REFERENCES Ausstattung(bezeichnung) ON DELETE CASCADE,
-    CONSTRAINT besitzt_fk2 FOREIGN KEY (ferienwohnungsname) REFERENCES Ferienwohnung(ferienwohnungsname) ON DELETE CASCADE
-);
-
-CREATE TABLE Touristenattraktion
-(
-    bezeichnung VARCHAR2(255) PRIMARY KEY,
-    beschreibung VARCHAR2(255) --Kann attribut
 );
 
 CREATE TABLE Ferienwohnung_Touristenattraktion
@@ -43,7 +70,6 @@ CREATE TABLE Ferienwohnung_Touristenattraktion
     CONSTRAINT ferienwohnung_touristen_fk1 FOREIGN KEY (ferienwohnungsName) REFERENCES Ferienwohnung(ferienwohnungsName) ON DELETE CASCADE,
     CONSTRAINT ferienwohnung_touristen_fk2 FOREIGN KEY (touristenattraktionsBezeichnung) REFERENCES Touristenattraktion(bezeichnung) ON DELETE CASCADE
 );
-
 CREATE TABLE Buchung
 (
     buchungsnr INT PRIMARY KEY,
@@ -64,33 +90,13 @@ CREATE TABLE Buchung
 ALTER TABLE Buchung ADD CONSTRAINT check_abreise_anreise CHECK (abreise > anreise);
 ALTER TABLE Buchung ADD CONSTRAINT check_bewertungsdatum CHECK (bewertungsdatum > anreise);
 
-CREATE TABLE Kunde
+CREATE TABLE Besitzt
 (
-    email VARCHAR2(255) PRIMARY KEY, 
-        CONSTRAINT email_format CHECK (REGEXP_LIKE(email, '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')),
-    vorname VARCHAR2(255) NOT NULL,
-    nachname VARCHAR2(255) NOT NULL,
-    IBAN VARCHAR2(255) NOT NULL,
-        constraint IBAN_foramat CHECK (REGEXP_LIKE(IBAN, '^[A-Z]{2}[0-9]{2}[A-Z0-9]{4}[0-9]{7}([A-Z0-9]?){0,16}$')),
-    passwort VARCHAR2(255) NOT NULL,
-    adressnr INT NOT NULL,
-    CONSTRAINT kunde_fk FOREIGN KEY (adressnr) REFERENCES Adresse(adressnr)
-);
-
-CREATE TABLE Land 
-(
-    bezeichnung VARCHAR2(255) PRIMARY KEY
-);
-CREATE TABLE Adresse
-(
-    adressnr INT PRIMARY KEY,
-    strasse VARCHAR2(255) NOT NULL,
-    hausnummer INT NOT NULL,
-        CONSTRAINT hausnummer_format CHECK (REGEXP_LIKE(hausnummer, '^[0-9]{1,5}$')),
-    plz INT NOT NULL CHECK (plz > 0),
-    ort VARCHAR2(255) NOT NULL CHECK (LENGTH(ort) > 2),
-    landbezeichnung VARCHAR2(255) NOT NULL,
-    CONSTRAINT adresse_fk FOREIGN KEY (landbezeichnung) REFERENCES Land(bezeichnung)
+    ausstatungsbezeichnung VARCHAR2(255) NOT NULL,
+    ferienwohnungsname VARCHAR2(255) NOT NULL CHECK (LENGTH(ferienwohnungsname) > 3),
+    CONSTRAINT besitzt_pk PRIMARY KEY (ausstatungsbezeichnung, ferienwohnungsname),
+    CONSTRAINT besitzt_fk1 FOREIGN KEY (ausstatungsbezeichnung) REFERENCES Ausstattung(bezeichnung) ON DELETE CASCADE,
+    CONSTRAINT besitzt_fk2 FOREIGN KEY (ferienwohnungsname) REFERENCES Ferienwohnung(ferienwohnungsname) ON DELETE CASCADE
 );
 
 CREATE TABLE Bezahlung
@@ -103,3 +109,4 @@ CREATE TABLE Bezahlung
 );
 
 COMMIT;
+
--- Dbsys/Aufgabe4/deleteTables.sql
+++ Dbsys/Aufgabe4/deleteTables.sql
@@ -1,1 +1,17 @@
-DROP TABLE LAND;
+
+
+DROP TABLE Besitzt;
+DROP TABLE Bezahlung;
+DROP TABLE Bilder;
+DROP TABLE Buchung;
+
+DROP TABLE Ferienwohnung_Touristenattraktion;
+DROP TABLE Kunde;
+
+DROP TABLE Touristenattraktion;
+
+
+DROP TABLE Ferienwohnung;
+DROP TABLE Adresse;
+DROP TABLE Land;
+DROP TABLE Ausstattung;
